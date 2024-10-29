fc64d4ac48f8a096202462d72e5f7a6e22c7dbc5
luthervalencia <147833078+stupidlystupid@users.noreply.github.com>
2024-10-29 14:50:44 +0800

    Update views.py

--- MiniFB/user/views.py
+++ MiniFB/user/views.py
@@ -10,7 +10,7 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned.data.get('username')
+            username = form.cleaned_data.get('username')
             messages.success(request, f'Accounnt created successfully for {username}.')
             return redirect('register')
     else:
